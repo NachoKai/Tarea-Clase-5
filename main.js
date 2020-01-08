da189a32437a0ac1e3c70bc58f25f07537da191c
NachoKai <47578311+NachoKai@users.noreply.github.com>
2020-01-08 20:24:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const $form = document.querySelector("#formulario")
-const $reset = document.querySelector('#reset')
-const $calcularResultado = document.querySelector('#boton-resultado')
+const $form = document.querySelector("#formulario"),
+    $reset = document.querySelector('#reset'),
+    $calcularResultado = document.querySelector('#boton-resultado')
 
 function reiniciar() {
     let nodoResultado = document.querySelector('#resultado')
@@ -12,10 +12,10 @@ calcularEdad = (anioActual, anioNacimiento) => anioActual - anioNacimiento
 calcularSalarioAnual = (salarioMensual) => (salarioMensual * 12)
 
 function mostrarResultado() {
-    let $anioActual = Number($form.anioActual.value)
-    let $anioNacimiento = Number($form.anioNacimiento.value)
-    let nodoResultado = document.querySelector('#resultado')
-    let edadActual = calcularEdad($anioActual, $anioNacimiento)
+    let $anioActual = Number($form.anioActual.value),
+        $anioNacimiento = Number($form.anioNacimiento.value),
+        nodoResultado = document.querySelector('#resultado'),
+        edadActual = calcularEdad($anioActual, $anioNacimiento)
 
     if (edadActual === 0) {
         nodoResultado.className = ('valido')
@@ -34,18 +34,19 @@ $calcularTodo.onclick = function () {
     calcularSalarioDiario = (salarioAnual) => (salarioAnual / diasLaborales)
     calcularSalarioHora = (salarioDiario) => (salarioDiario / horasTrabajo)
 
-    let horasTrabajo = Number(document.querySelector("#horas-trabajo").value);
-    let diasVacaciones = Number(document.querySelector("#dias-vacaciones").value);
-    let diasLaborales = 365 - diasVacaciones
-    let salarioMensual = Number(document.querySelector("#salario-mensual").value);
-    let salarioAnual = calcularSalarioAnual(salarioMensual);
-    let salarioDiario = calcularSalarioDiario(salarioAnual);
-    let salarioHora = calcularSalarioHora(salarioDiario);
-    let anioActual = Number(document.querySelector("#anio-actual").value)
-    let anioNacimiento = Number(document.querySelector("#anio-nacimiento").value);
-    let edadActual = calcularEdad(anioActual, anioNacimiento);
-    let nombreUsuario = document.querySelector("#nombre-usuario").value
-    let nombreUsuarioMayuscula = nombreUsuario.charAt(0).toUpperCase() + nombreUsuario.slice(1)
+    let horasTrabajo = Number(document.querySelector("#horas-trabajo").value),
+        diasVacaciones = Number(document.querySelector("#dias-vacaciones").value),
+        diasLaborales = 365 - diasVacaciones,
+        salarioMensual = Number(document.querySelector("#salario-mensual").value),
+        salarioAnual = calcularSalarioAnual(salarioMensual),
+        salarioDiario = calcularSalarioDiario(salarioAnual),
+        salarioHora = calcularSalarioHora(salarioDiario),
+        anioActual = Number(document.querySelector("#anio-actual").value),
+        anioNacimiento = Number(document.querySelector("#anio-nacimiento").value),
+        edadActual = calcularEdad(anioActual, anioNacimiento),
+        nombreUsuario = document.querySelector("#nombre-usuario").value,
+        nombreUsuarioMayuscula = nombreUsuario.charAt(0).toUpperCase() + nombreUsuario.slice(1)
+
     document.querySelector("#resultado").className = 'valido'
     document.querySelector("#resultado").innerText =
         `Hola ${nombreUsuarioMayuscula}! Naciste en el año ${anioNacimiento} y estamos en el año ${anioActual}, por lo tanto tu edad aproximada es de ${edadActual} años. Tu salario mensual es de: $${salarioMensual}. Tu salario anual es de $${salarioAnual}. Tu salario diario es de $${salarioDiario.toFixed(0)}. Y tu salario por hora es de $${salarioHora.toFixed(0)}.`;
